0d96c09e601a458af1600b617c9b58f685b5d4ee
Maksym Naumchenko <67383097+skv11d@users.noreply.github.com>
2025-03-16 13:33:09 +0100

    Add files via upload

---
+++ posListe.py
@@ -0,0 +1,16 @@
+liste = [1, "a", 0, 0, "a", 1, 1, "a", 0, 1, 0, "a"]
+
+element = input()
+zaehler = -1
+posListe = []
+
+if element.isdigit():
+    element = int(element)
+
+for i in range(len(liste)):
+    zaehler += 1
+    if liste[i] == element:
+        posListe += [zaehler]
+
+print("Das Element", element, "kommt zum ersten Mal an Position", posListe[0], "vor und zum letzten Mal an Position", posListe[len(posListe) - 1])
+    
